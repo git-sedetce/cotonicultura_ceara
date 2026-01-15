202b60fdd5661e38f97dff7a9586b2bc5e4be310
Daniel Araújo <daniel.araujo@sde.ce.gov.br>
2026-01-15 17:34:18 -0300

    componente listar usuários

---
+++ client/src/app/components/lista-user/lista-user.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-user',
+  standalone: false,
+  templateUrl: './lista-user.component.html',
+  styleUrl: './lista-user.component.css'
+})
+export class ListaUserComponent {
+
+}
