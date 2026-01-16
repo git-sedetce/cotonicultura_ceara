9519d5848ff8b82609a5bfecb00c66cd8520155a
Daniel Araújo <daniel.araujo@sde.ce.gov.br>
2026-01-16 13:24:56 -0300

    componente quem somos

---
+++ client/src/app/components/who-are/who-are.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-who-are',
+  standalone: false,
+  templateUrl: './who-are.component.html',
+  styleUrl: './who-are.component.css'
+})
+export class WhoAreComponent {
+
+}
