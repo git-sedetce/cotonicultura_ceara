437cc91a3f20093c51928a49a8f0bc82197219c5
Daniel Oliveira Araújo <danieloliveira14@gmail.com>
2026-01-29 20:13:41 -0300

    validar os anexos e a área de plantio

--- client/src/app/components/cadastro-produtor/cadastro-produtor.component.ts
+++ client/src/app/components/cadastro-produtor/cadastro-produtor.component.ts
@@ -11,7 +11,7 @@ import { HttpClient } from '@angular/common/http';
   selector: 'app-cadastro-produtor',
   standalone: false,
   templateUrl: './cadastro-produtor.component.html',
-  styleUrl: './cadastro-produtor.component.css'
+  styleUrl: './cadastro-produtor.component.css',
 })
 export class CadastroProdutorComponent implements OnInit {
   @ViewChild('formAgricultor') formAgricultor!: NgForm;
@@ -20,111 +20,146 @@ export class CadastroProdutorComponent implements OnInit {
   agricultor!: Agricultor;
   comprovanteSelected: boolean = false;
   cpf_cnpjSelected: boolean = false;
+  arquivoResidenciaInvalido = false;
+  arquivoDocumentoInvalido = false;
   city_list!: any[];
 
   constructor(
     private router: Router,
     private http: HttpClient,
     private toastr: ToastrService,
-    private cadastroAgricultorService: CadastroAgricultorService
-  ) { }
+    private cadastroAgricultorService: CadastroAgricultorService,
+  ) {}
 
   ngOnInit(): void {
     this.agricultor = new Agricultor();
     this.getCity();
   }
 
-  getCity(){
-    this.cadastroAgricultorService.getCitys('takecitys').subscribe((cityBr: any[]) => {
-      // console.log('cityBr', cityBr)
-      this.city_list = cityBr;
-    }, (erro: any) => console.error('erro', erro)
+  getCity() {
+    this.cadastroAgricultorService.getCitys('takecitys').subscribe(
+      (cityBr: any[]) => {
+        // console.log('cityBr', cityBr)
+        this.city_list = cityBr;
+      },
+      (erro: any) => console.error('erro', erro),
     );
   }
 
-  consultaCPF(epf:any, form: any){
-      this.cadastroAgricultorService.consultarCPF(epf).subscribe((res:any) =>{
-        if(res.mensagem === 'CPF/CNPJ já cadastrado!'){
-          this.toastr.error(res.mensagem)
-          this.formAgricultor.reset();
-        }
-      })
-    }
-
-    consultaADAGRI(adagri:any, form: any){
-      this.cadastroAgricultorService.consultarADAGRI(adagri).subscribe((res:any) =>{
-        if(res.mensagem === 'Cadastro ADAGRI já cadastrado!'){
-          this.toastr.error(res.mensagem)
-          this.formAgricultor.reset();
-        }
-      })
-    }
-
-  cadastrarAgricultor(){
-    this.cadastroAgricultorService.cadastrarAgricultor(this.agricultor).subscribe({
-      next: (res: any) => {
-        // console.log('res', res)
-        this.onDocumentoUpload(res.id);
-        this.onComprovanteUpload(res.id);
-        this.toastr.success('Cadastro realizado com sucesso!');
-        // this.cadastroAgricultorService.setAgricultor(Number(res.id));
+  consultaCPF(epf: any, form: any) {
+    this.cadastroAgricultorService.consultarCPF(epf).subscribe((res: any) => {
+      if (res.mensagem === 'CPF/CNPJ já cadastrado!') {
+        this.toastr.error(res.mensagem);
         this.formAgricultor.reset();
-        this.router.navigate(['/home'])
-      },
-      error: (e) => console.error('erro',e)//(this.toastr.error(e.message))
+      }
     });
+  }
+
+  consultaADAGRI(adagri: any, form: any) {
+    this.cadastroAgricultorService
+      .consultarADAGRI(adagri)
+      .subscribe((res: any) => {
+        if (res.mensagem === 'Cadastro ADAGRI já cadastrado!') {
+          this.toastr.error(res.mensagem);
+          this.formAgricultor.reset();
+        }
+      });
+  }
+
+  cadastrarAgricultor() {
+
+    if (!this.anexoCPFCNPJ?.nativeElement.files.length) {
+    this.arquivoDocumentoInvalido = true;
+    return;
+  }
+
+  if (!this.anexoResidencia?.nativeElement.files.length) {
+    this.arquivoResidenciaInvalido = true;
+    return;
+  }
+    this.cadastroAgricultorService
+      .cadastrarAgricultor(this.agricultor)
+      .subscribe({
+        next: (res: any) => {
+          // console.log('res', res)
+          this.onDocumentoUpload(res.id);
+          this.onComprovanteUpload(res.id);
+          this.toastr.success('Cadastro realizado com sucesso!');
+          // this.cadastroAgricultorService.setAgricultor(Number(res.id));
+          this.formAgricultor.reset();
+          this.router.navigate(['/home']);
+        },
+        error: (e) => console.error('erro', e), //(this.toastr.error(e.message))
+      });
     // this.saveRegister();
   }
 
   onCPFCNPJSelected(event: any): void {
-    this.cpf_cnpjSelected = event.target.files.length > 0;
-    // console.log('comprovanteSelected', this.comprovanteSelected);
+    const input = event.target as HTMLInputElement;
+
+    if (!input.files || input.files.length === 0) {
+      this.arquivoDocumentoInvalido = true;
+      return;
+    }
+
+    this.arquivoDocumentoInvalido = false;
+
+    const arquivo = input.files[0];
   }
 
   //anexar arquivos
   onDocumentoUpload(id: any) {
     const imageDoc = this.anexoCPFCNPJ.nativeElement.files[0];
+    if (!imageDoc) return;
     const file = new FormData();
     file.append('file', imageDoc);
     file.append('id', id);
     //console.log('formData', file)
     //console.log('id', user_id)
 
-    this.http.post(environment.apiUrl + 'anexoCPFCNPJ' + '/' + id, file).subscribe({
-      next: (response: any) => {
-        this.toastr.success('Comprovante de CPF/CNPJ anexado com sucesso!');
-        console.log('resposta_anexo', response);
-      },
-      error: (e) => {
-        this.toastr.error(e.error.message);
-        console.log('resposta_anexo', e);
-      },
-    });
+    this.http
+      .post(environment.apiUrl + 'anexoCPFCNPJ' + '/' + id, file)
+      .subscribe({
+        next: (response: any) => {
+          this.toastr.success('Comprovante de CPF/CNPJ anexado com sucesso!');
+        },
+        error: (e) => {
+          this.toastr.error(e.error.message);
+        },
+      });
   }
 
   onComprovanteSelected(event: any): void {
-    this.comprovanteSelected = event.target.files.length > 0;
-    // console.log('comprovanteSelected', this.comprovanteSelected);
+    const input = event.target as HTMLInputElement;
+
+    if (!input.files || input.files.length === 0) {
+      this.arquivoResidenciaInvalido = true;
+      return;
+    }
+
+    this.arquivoResidenciaInvalido = false;
+
+    const arquivo = input.files[0];
   }
 
   onComprovanteUpload(id: any) {
     const imageRes = this.anexoResidencia.nativeElement.files[0];
+    if (!imageRes) return;
     const file = new FormData();
     file.append('file', imageRes);
     file.append('id', id);
     //console.log('formData', file)
     //console.log('id', user_id)
 
-    this.http.post(environment.apiUrl + 'anexoResidencia' + '/' + id, file).subscribe({
-      next: (response: any) => {
-        this.toastr.success('Comprovante de Residência anexado com sucesso!');
-        console.log('resposta_anexo', response);
-      },
-      error: (e) => {
-        this.toastr.error(e.error.message);
-        console.log('resposta_anexo', e);
-      },
-    });
+    this.http
+      .post(environment.apiUrl + 'anexoResidencia' + '/' + id, file)
+      .subscribe({
+        next: (response: any) => {
+          this.toastr.success('Comprovante de Residência anexado com sucesso!');
+        },
+        error: (e) => {
+          this.toastr.error(e.error.message);
+        },
+      });
   }
-
 }
